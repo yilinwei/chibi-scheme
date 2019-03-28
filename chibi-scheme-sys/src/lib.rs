9d531836560319bdad8fe8336b18a41d985fdd9d
Yilin <yilin.wei10@gmail.com>
2019-03-28 00:34:37 +0000

    Add sexp.rs

---
+++ chibi-scheme-sys/src/lib.rs
@@ -0,0 +1,99 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[macro_export]
+macro_rules! sexp_make_immediate {
+    ($n: expr) => {
+        (($n << SEXP_EXTENDED_BITS) + SEXP_EXTENDED_TAG) as sexp
+    };
+}
+
+#[macro_export]
+macro_rules! i8_cstring {
+    ($s: expr) => {
+        concat!($s, "\0").as_ptr() as *const i8
+    };
+}
+
+#[macro_export]
+macro_rules! sexp_unbox_fixnum {
+    ($n: expr) => {
+        ($n as sexp_sint_t) >> SEXP_FIXNUM_BITS
+    };
+}
+
+pub const SEXP_FALSE: sexp = sexp_make_immediate!(0);
+pub const SEXP_TRUE: sexp = sexp_make_immediate!(1);
+pub const SEXP_NULL: sexp = sexp_make_immediate!(2);
+pub const SEXP_EOF: sexp = sexp_make_immediate!(3);
+pub const SEXP_VOID: sexp = sexp_make_immediate!(4);
+
+#[macro_export]
+macro_rules! sexp_truep {
+    ($x: expr) => {
+        $x != SEXP_FALSE
+    };
+}
+
+#[macro_export]
+macro_rules! sexp_not {
+    ($x: expr) => {
+        $x == SEXP_FALSE
+    };
+}
+
+#[macro_export]
+macro_rules! sexp_nullp {
+    ($x: expr) => {
+        $x == SEXP_NULL
+    };
+}
+
+#[macro_export]
+macro_rules! sexp_fixnump {
+    ($x: expr) => {
+        (($x as sexp_uint_t) & SEXP_FIXNUM_MASK as u64) == SEXP_FIXNUM_TAG as u32
+    };
+}
+
+#[macro_export]
+macro_rules! sexp_isymbolp {
+    ($x: expr) => {
+        (($x as sexp_uint_t) & SEXP_IMMEDIATE_MASK) == SEXP_ISYMBOL_TAG as u32
+    };
+}
+
+#[macro_export]
+macro_rules! sexp_charp {
+    ($x: expr) => {
+        (($x as sexp_uint_t) & SEXP_EXTENDED_MASK) == SEXP_CHAR_TAG as u64
+    };
+}
+
+#[macro_export]
+macro_rules! sexp_booleanp {
+    ($x: expr) => {
+        (($x) == SEXP_TRUE) || (($x) == SEXP_FALSE)
+    }
+}
+
+fn eval(expr: *const i8) -> sexp {
+    unsafe {
+        let ctx = sexp_make_eval_context(
+            std::ptr::null_mut(),
+            std::ptr::null_mut(),
+            std::ptr::null_mut(),
+            0,
+            0,
+        );
+        sexp_eval_string(ctx, expr, -1, std::ptr::null_mut())
+    }
+}
+
+#[test]
+fn test_unbox_fixnum() {
+    assert_eq!(10, sexp_unbox_fixnum!(eval(i8_cstring!("(+ 3 7)"))))
+}
---
+++ src/sexp.rs
@@ -0,0 +1,47 @@
+use chibi_scheme_sys::*;
+
+use std::fmt;
+
+struct SExp(sexp);
+
+impl SExp {
+    fn bool(self) -> Option<Bool> {
+        if sexp_booleanp!(self.0) {
+            Some(Bool(self.0))
+        } else {
+            None
+        }
+    }
+}
+
+struct Symbol(sexp);
+
+struct Char(sexp);
+
+struct Bool(sexp);
+
+impl Into<bool> for Bool {
+    fn into(self: Self) -> bool {
+        sexp_truep!(self.0)
+    }
+}
+
+impl From<bool> for Bool {
+    fn from(b: bool) -> Bool {
+        if b {
+            Bool(SEXP_TRUE)
+        } else {
+            Bool(SEXP_FALSE)
+        }
+    }
+}
+
+impl fmt::Debug for Bool {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        if sexp_truep!(self.0) {
+            fmt.write_str("#t")
+        } else {
+            fmt.write_str("#f")
+        }
+    }
+}
