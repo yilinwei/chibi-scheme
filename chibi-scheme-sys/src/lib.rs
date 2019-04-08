c601912157e4597ea4900879b95321eed5cbb45d
Yilin <yilin.wei10@gmail.com>
2019-04-08 18:23:07 +0100

    Add string methods

--- chibi-scheme-sys/src/lib.rs
+++ chibi-scheme-sys/src/lib.rs
@@ -4,92 +4,98 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-#[macro_export]
-macro_rules! sexp_make_immediate {
-    ($n: expr) => {
-        (($n << $crate::SEXP_EXTENDED_BITS) + $crate::SEXP_EXTENDED_TAG) as $crate::sexp
-    };
+use std::os::raw;
+
+const fn sexp_make_immediate(n: u32) -> sexp {
+    ((n << SEXP_EXTENDED_BITS) + SEXP_EXTENDED_TAG) as sexp
 }
 
-#[macro_export]
-macro_rules! i8_cstring {
-    ($s: expr) => {
-        concat!($s, "\0").as_ptr() as *const i8
-    };
+pub fn sexp_unbox_fixnum(n: sexp) -> sexp_sint_t {
+    (n as sexp_sint_t) >> SEXP_FIXNUM_BITS
 }
 
-#[macro_export]
-macro_rules! sexp_unbox_fixnum {
-    ($n: expr) => {
-        ($n as $crate::sexp_sint_t) >> $crate::SEXP_FIXNUM_BITS
-    };
+pub fn sexp_make_character(n: raw::c_char) -> sexp {
+    (((n as sexp_sint_t) << SEXP_EXTENDED_BITS) + (SEXP_CHAR_TAG as sexp_sint_t)) as sexp
 }
 
-#[macro_export]
-macro_rules! sexp_make_character {
-    ($n: expr) => {
-        ((($n as $crate::sexp_sint_t) << $crate::SEXP_EXTENDED_BITS) + ($crate::SEXP_CHAR_TAG as $crate::sexp_sint_t)) as $crate::sexp
-    }
+pub fn sexp_unbox_character(n: sexp) -> raw::c_char {
+    ((n as sexp_sint_t) >> SEXP_EXTENDED_BITS) as raw::c_char
 }
 
-#[macro_export]
-macro_rules! sexp_unbox_character {
-    ($n: expr) => {
-        (($n as $crate::sexp_sint_t) >> $crate::SEXP_EXTENDED_BITS) as std::os::raw::c_char
-    }
+pub const SEXP_FALSE: sexp = sexp_make_immediate(0);
+pub const SEXP_TRUE: sexp = sexp_make_immediate(1);
+pub const SEXP_NULL: sexp = sexp_make_immediate(2);
+pub const SEXP_EOF: sexp = sexp_make_immediate(3);
+pub const SEXP_VOID: sexp = sexp_make_immediate(4);
+
+pub fn sexp_truep(x: sexp) -> bool {
+    x != SEXP_FALSE
 }
 
-pub const SEXP_FALSE: sexp = sexp_make_immediate!(0);
-pub const SEXP_TRUE: sexp = sexp_make_immediate!(1);
-pub const SEXP_NULL: sexp = sexp_make_immediate!(2);
-pub const SEXP_EOF: sexp = sexp_make_immediate!(3);
-pub const SEXP_VOID: sexp = sexp_make_immediate!(4);
-
-#[macro_export]
-macro_rules! sexp_truep {
-    ($x: expr) => {
-        $x != $crate::SEXP_FALSE
-    };
+pub fn sexp_not(x: sexp) -> bool {
+    x == SEXP_FALSE
 }
 
-#[macro_export]
-macro_rules! sexp_not {
-    ($x: expr) => {
-        $x == $crate::SEXP_FALSE
-    };
+pub fn sexp_nullp(x: sexp) -> bool {
+    x == SEXP_NULL
 }
 
-#[macro_export]
-macro_rules! sexp_nullp {
-    ($x: expr) => {
-        $x == $crate::SEXP_NULL
-    };
+pub fn sexp_fixnump(x: sexp) -> bool {
+    ((x as sexp_uint_t) & SEXP_FIXNUM_MASK as sexp_uint_t) == SEXP_FIXNUM_TAG as sexp_uint_t
 }
 
-#[macro_export]
-macro_rules! sexp_fixnump {
-    ($x: expr) => {
-        (($x as sexp_uint_t) & SEXP_FIXNUM_MASK as u64) == SEXP_FIXNUM_TAG as u32
-    };
+pub fn sexp_isymbolp(x: sexp) -> bool {
+    ((x as sexp_uint_t) & SEXP_IMMEDIATE_MASK as sexp_uint_t) == SEXP_ISYMBOL_TAG as sexp_uint_t
 }
 
-#[macro_export]
-macro_rules! sexp_isymbolp {
-    ($x: expr) => {
-        (($x as sexp_uint_t) & SEXP_IMMEDIATE_MASK) == SEXP_ISYMBOL_TAG as u32
-    };
+pub fn sexp_charp(x: sexp) -> bool {
+    ((x as sexp_uint_t) & SEXP_EXTENDED_MASK as sexp_uint_t) == SEXP_CHAR_TAG as sexp_uint_t
 }
 
-#[macro_export]
-macro_rules! sexp_charp {
-    ($x: expr) => {
-        (($x as $crate::sexp_uint_t) & (SEXP_EXTENDED_MASK as $crate::sexp_uint_t)) == SEXP_CHAR_TAG as $crate::sexp_uint_t
-    };
+pub fn sexp_booleanp(x: sexp) -> bool {
+    x == SEXP_TRUE || x == SEXP_FALSE
 }
 
-#[macro_export]
-macro_rules! sexp_booleanp {
-    ($x: expr) => {
-        (($x) == $crate::SEXP_TRUE) || (($x) == $crate::SEXP_FALSE)
-    }
+pub fn sexp_pointerp(x: sexp) -> bool {
+    ((x as sexp_uint_t) & SEXP_POINTER_MASK as sexp_uint_t) == SEXP_POINTER_TAG as sexp_uint_t
 }
+
+pub fn sexp_pointer_tag(x: sexp) -> sexp_tag_t {
+    unsafe { (*x).tag }
+}
+
+pub fn sexp_check_tag(x: sexp, t: sexp_tag_t) -> bool {
+    sexp_pointerp(x) &&
+        (sexp_pointer_tag(x) == t)
+}
+
+pub fn sexp_stringp(x: sexp) -> bool {
+    sexp_check_tag(x, sexp_types_SEXP_STRING)
+}
+
+pub fn sexp_string_size(x: sexp) -> sexp_uint_t {
+    unsafe { (*x).value.string.as_ref().length }
+}
+
+pub fn sexp_bytes_length(x: sexp) -> sexp_uint_t {
+    unsafe { (*x).value.bytes.as_ref().length }
+}
+
+pub fn sexp_bytes_data(x: sexp) -> *const raw::c_char {
+    unsafe { (*x).value.bytes.as_ref().data.as_ptr() }
+}
+
+pub fn sexp_string_offset(x: sexp) -> sexp_uint_t {
+    unsafe { (*x).value.string.as_ref().offset }
+}
+
+pub fn sexp_string_data(x: sexp) -> *const raw::c_char {
+    unsafe{ sexp_bytes_data((*x).value.string.as_ref().bytes).offset(sexp_string_offset(x) as isize) }
+}
+
+pub fn sexp_string_length(x: sexp) -> sexp_uint_t {
+    unsafe{ (*x).value.string.as_ref().length }
+}
+
+// TODO: Safe accessor
+// TODO: Add feature for stuff
--- src/sexp.rs
+++ src/sexp.rs
@@ -1,13 +1,15 @@
 use chibi_scheme_sys::*;
 
+use std::ffi;
 use std::fmt;
 use std::os::raw;
+use std::slice;
 
 pub struct SExp(pub sexp);
 
 impl SExp {
     pub fn bool(self) -> Option<Bool> {
-        if sexp_booleanp!(self.0) {
+        if sexp_booleanp(self.0) {
             Some(Bool(self.0))
         } else {
             None
@@ -15,11 +17,52 @@ impl SExp {
     }
 
     pub fn char(self) -> Option<Char> {
-        if sexp_charp!(self.0) {
+        if sexp_charp(self.0) {
             Some(Char(self.0))
         } else {
             None
         }
+    }
+
+    pub fn string(self) -> Option<String> {
+        if sexp_stringp(self.0) {
+            Some(String(self.0))
+        } else {
+            None
+        }
+    }
+}
+
+pub struct String(sexp);
+
+impl String {
+    // TODO: Once lifetime has worked, then remove
+    fn as_c_str(&self) -> &ffi::CStr {
+        let len = (sexp_string_size(self.0) + 1) as _;
+        let slice = unsafe { slice::from_raw_parts(sexp_string_data(self.0) as _, len) };
+        unsafe { ffi::CStr::from_bytes_with_nul_unchecked(slice) }
+    }
+}
+
+impl Into<ffi::CString> for String {
+    fn into(self: Self) -> ffi::CString {
+        let len = (sexp_string_size(self.0) + 1) as _;
+        let mut data = Vec::with_capacity(len);
+        let slice = unsafe { slice::from_raw_parts(sexp_string_data(self.0) as _, len) };
+        data.extend_from_slice(slice);
+        unsafe { ffi::CString::from_vec_unchecked(data) }
+    }
+}
+
+impl fmt::Debug for String {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        fmt.write_str(&format!("\"{:?}\"", self.as_c_str()))
+    }
+}
+
+impl PartialEq for String {
+    fn eq(self: &Self, rhs: &Self) -> bool {
+        self.as_c_str() == rhs.as_c_str()
     }
 }
 
@@ -30,27 +73,27 @@ pub struct Char(sexp);
 impl Into<char> for Char {
     fn into(self: Self) -> char {
         //TODO: Need to check casting
-        (sexp_unbox_character!(self.0) as u8) as char
+        (sexp_unbox_character(self.0) as u8) as char
     }
 }
 
 impl From<char> for Char {
     fn from(c: char) -> Char {
-        //TODO: safety first -
-        Char(sexp_make_character!(c as raw::c_char))
+        //TODO: check
+        Char(sexp_make_character(c as _))
     }
 }
 
 impl fmt::Debug for Char {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        fmt.write_str(&format!("#\\{}", sexp_unbox_character!(self.0 as u8)))
+        fmt.write_str(&format!("#\\{}", sexp_unbox_character(self.0)))
     }
 }
 
 impl PartialEq for Char {
     fn eq(self: &Self, rhs: &Self) -> bool {
-        sexp_unbox_character!(self.0 as u8) ==
-            sexp_unbox_character!(rhs.0 as u8)
+        sexp_unbox_character(self.0) ==
+            sexp_unbox_character(rhs.0)
     }
 }
 
@@ -58,7 +101,7 @@ pub struct Bool(sexp);
 
 impl Into<bool> for Bool {
     fn into(self: Self) -> bool {
-        sexp_truep!(self.0)
+        sexp_truep(self.0)
     }
 }
 
@@ -74,14 +117,14 @@ impl From<bool> for Bool {
 
 impl PartialEq for Bool {
     fn eq(self: &Self, rhs: &Self) -> bool {
-        sexp_truep!(self.0) && sexp_truep!(rhs.0)
-            || sexp_not!(self.0) && sexp_not!(rhs.0)
+        sexp_truep(self.0) && sexp_truep(rhs.0)
+            || sexp_not(self.0) && sexp_not(rhs.0)
     }
 }
 
 impl fmt::Debug for Bool {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        if sexp_truep!(self.0) {
+        if sexp_truep(self.0) {
             fmt.write_str("#t")
         } else {
             fmt.write_str("#f")
