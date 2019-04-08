a51c5cfca9b02e9275d05b5df5b0a36d6a72f38f
Yilin <yilin.wei10@gmail.com>
2019-04-08 14:25:51 +0100

    Add scoping for variables

--- chibi-scheme-sys/src/lib.rs
+++ chibi-scheme-sys/src/lib.rs
@@ -7,7 +7,7 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 #[macro_export]
 macro_rules! sexp_make_immediate {
     ($n: expr) => {
-        (($n << SEXP_EXTENDED_BITS) + SEXP_EXTENDED_TAG) as sexp
+        (($n << $crate::SEXP_EXTENDED_BITS) + $crate::SEXP_EXTENDED_TAG) as $crate::sexp
     };
 }
 
@@ -21,21 +21,21 @@ macro_rules! i8_cstring {
 #[macro_export]
 macro_rules! sexp_unbox_fixnum {
     ($n: expr) => {
-        ($n as sexp_sint_t) >> SEXP_FIXNUM_BITS
+        ($n as $crate::sexp_sint_t) >> $crate::SEXP_FIXNUM_BITS
     };
 }
 
 #[macro_export]
 macro_rules! sexp_make_character {
     ($n: expr) => {
-        ((($n as sexp_sint_t) << SEXP_EXTENDED_BITS) + (SEXP_CHAR_TAG as sexp_sint_t)) as sexp
+        ((($n as $crate::sexp_sint_t) << $crate::SEXP_EXTENDED_BITS) + ($crate::SEXP_CHAR_TAG as $crate::sexp_sint_t)) as $crate::sexp
     }
 }
 
 #[macro_export]
 macro_rules! sexp_unbox_character {
     ($n: expr) => {
-        (($n as sexp_sint_t) >> SEXP_EXTENDED_BITS) as std::os::raw::c_char
+        (($n as $crate::sexp_sint_t) >> $crate::SEXP_EXTENDED_BITS) as std::os::raw::c_char
     }
 }
 
@@ -48,21 +48,21 @@ pub const SEXP_VOID: sexp = sexp_make_immediate!(4);
 #[macro_export]
 macro_rules! sexp_truep {
     ($x: expr) => {
-        $x != SEXP_FALSE
+        $x != $crate::SEXP_FALSE
     };
 }
 
 #[macro_export]
 macro_rules! sexp_not {
     ($x: expr) => {
-        $x == SEXP_FALSE
+        $x == $crate::SEXP_FALSE
     };
 }
 
 #[macro_export]
 macro_rules! sexp_nullp {
     ($x: expr) => {
-        $x == SEXP_NULL
+        $x == $crate::SEXP_NULL
     };
 }
 
@@ -83,13 +83,13 @@ macro_rules! sexp_isymbolp {
 #[macro_export]
 macro_rules! sexp_charp {
     ($x: expr) => {
-        (($x as sexp_uint_t) & (SEXP_EXTENDED_MASK as sexp_uint_t)) == SEXP_CHAR_TAG as sexp_uint_t
+        (($x as $crate::sexp_uint_t) & (SEXP_EXTENDED_MASK as $crate::sexp_uint_t)) == SEXP_CHAR_TAG as $crate::sexp_uint_t
     };
 }
 
 #[macro_export]
 macro_rules! sexp_booleanp {
     ($x: expr) => {
-        (($x) == SEXP_TRUE) || (($x) == SEXP_FALSE)
+        (($x) == $crate::SEXP_TRUE) || (($x) == $crate::SEXP_FALSE)
     }
 }
