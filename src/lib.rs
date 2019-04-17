8cb96e8d0e958f9655a2ac39b7c6636f0fd7ceb1
zainab-ali <zainab.ali.london@gmail.com>
2019-04-17 12:42:40 +0100

    Add integers and rationals

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ use std::string;
 pub struct RawSExp<'a>(sexp, Option<&'a RawContext>);
 
 // TODO: Add generic typing please
-pub type Coprod<'a> = frunk::Coprod!(Bool, Char, Null, Pair<'a>, String<'a>);
+pub type Coprod<'a> = frunk::Coprod!(Bool, Char, Integer, Rational<'a>, Null, Pair<'a>, String<'a>, Exception<'a>, Symbol<'a>);
 
 //
 pub struct SExp<'a>(Coprod<'a>);
@@ -39,9 +39,13 @@ impl<'a> fmt::Debug for SExp<'a> {
         self.0.to_ref().fold(frunk::hlist![
             |b: &Bool| b.fmt(&mut cell.borrow_mut()),
             |c: &Char| c.fmt(&mut cell.borrow_mut()),
+            |i: &Integer| i.fmt(&mut cell.borrow_mut()),
+            |i: &Rational| i.fmt(&mut cell.borrow_mut()),
             |n: &Null| n.fmt(&mut cell.borrow_mut()),
             |p: &Pair<'a>| p.fmt(&mut cell.borrow_mut()),
-            |s: &String<'a>| s.fmt(&mut cell.borrow_mut())
+            |s: &String<'a>| s.fmt(&mut cell.borrow_mut()),
+            |s: &Exception<'a>| s.fmt(&mut cell.borrow_mut()),
+            |s: &Symbol<'a>| s.fmt(&mut cell.borrow_mut())
         ])
     }
 }
@@ -93,8 +97,12 @@ impl<'a> fmt::Debug for Pair<'a> {
             while t.0.to_ref().fold(frunk::hlist![
                 |_| unreachable!(),
                 |_| unreachable!(),
+                |_| unreachable!(),
+                |_| unreachable!(),
                 |_: &Null| false,
                 |_: &Pair| true,
+                |_| unreachable!(),
+                |_| unreachable!(),
                 |_| unreachable!()
             ]) {
                 h.fmt(fmt)?;
@@ -126,7 +134,20 @@ impl PartialEq for Null {
     }
 }
 
-pub struct Symbol(sexp);
+pub struct Symbol<'a>(RawSExp<'a>);
+
+impl<'a> PartialEq for Symbol<'a> {
+    fn eq(self: &Self, rhs: &Self) -> bool {
+        (self.0).1.unwrap().equalp(&self.0, &rhs.0)
+    }
+}
+
+impl<'a> fmt::Debug for Symbol<'a> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let s = (self.0).1.unwrap().symbol_to_string(&self.0);
+        fmt.write_fmt(format_args!("{:?}", s.data()))
+    }
+}
 
 pub struct Char(RawSExp<'static>);
 
@@ -196,11 +217,79 @@ impl fmt::Debug for Bool {
     }
 }
 
+pub struct Integer(RawSExp<'static>);
+
+impl Into<i64> for Integer {
+    fn into(self: Self) -> i64 {
+        //TODO: Need to check casting
+        (sexp_unbox_fixnum((self.0).0) as u8) as i64
+    }
+}
+
+impl From<i64> for Integer {
+    fn from(i: i64) -> Integer {
+        //TODO: check
+        Integer(RawSExp(sexp_make_fixnum(i as _), None))
+    }
+}
+
+impl fmt::Debug for Integer {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        fmt.write_fmt(format_args!("{}", RawContext::unbox_fixnum(&self.0)))
+    }
+}
+
+impl PartialEq for Integer {
+    fn eq(self: &Self, rhs: &Self) -> bool {
+        RawContext::unbox_fixnum(&self.0) as i64 == RawContext::unbox_fixnum(&rhs.0)
+    }
+}
+
+pub struct Rational<'a>(RawSExp<'a>);
+
+impl<'a> Into<f64> for Rational<'a> {
+
+    fn into(self: Self) -> f64 {
+        sexp_flonum_value((self.0).0)
+    }
+}
+
+impl<'a> fmt::Debug for Rational<'a> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let f: f64 = sexp_flonum_value((self.0).0);
+        fmt.write_fmt(format_args!("{}", f))
+    }
+}
+
+impl<'a> PartialEq for Rational<'a> {
+    fn eq(self: &Self, rhs: &Self) -> bool {
+        sexp_flonum_value((&self.0).0) as f64 == sexp_flonum_value((&rhs.0).0) as f64
+    }
+}
+
+pub struct Exception<'a>(RawSExp<'a>);
+
+impl<'a> fmt::Debug for Exception<'a> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let message_string = String((self.0).1.unwrap().exception_message(&self.0));
+        fmt.write_fmt(format_args!("Exception: {:?}", message_string))
+    }
+}
+
 struct RawContext(sexp);
 
 impl RawContext {
     fn unbox_character(a: &RawSExp) -> char {
         (sexp_unbox_character(a.0) as u8) as char
+    }
+
+    fn unbox_fixnum(a: &RawSExp) -> i64 {
+        (sexp_unbox_fixnum(a.0) as u8) as i64
+    }
+
+    fn make_flonum(&self, i: f64) -> RawSExp {
+        let flonum = unsafe { sexp_make_flonum(self.0, i)};
+        RawSExp(flonum, None)
     }
 
     fn string_data<'a>(a: &'a RawSExp) -> &'a ffi::CStr {
@@ -209,6 +298,11 @@ impl RawContext {
         unsafe { ffi::CStr::from_bytes_with_nul_unchecked(slice) }
 
     }
+
+    fn exception_message(&self, a: &RawSExp) -> RawSExp {
+        RawSExp(sexp_exception_message(a.0), Some(self))
+    }
+
     fn string_size(a: &RawSExp) -> usize {
         sexp_string_size(a.0) as usize
     }
@@ -240,6 +334,14 @@ impl RawContext {
         sexp_charp(sexp.0)
     }
 
+    fn integerp(sexp: &RawSExp) -> bool {
+        sexp_integerp(sexp.0)
+    }
+
+    fn flonump(sexp: &RawSExp) -> bool {
+        sexp_flonump(sexp.0)
+    }
+
     fn stringp(sexp: &RawSExp) -> bool {
         sexp_stringp(sexp.0)
     }
@@ -260,12 +362,24 @@ impl RawContext {
         sexp_pairp(sexp.0)
     }
 
+    fn symbolp(sexp: &RawSExp) -> bool {
+        sexp_isymbolp(sexp.0) || sexp_lsymbolp(sexp.0)
+    }
+
+    fn exceptionp(sexp: &RawSExp) -> bool {
+        sexp_exceptionp(sexp.0)
+    }
+
     fn eval_string<T: Into<string::String>>(&self, t: T) -> Result<RawSExp, ffi::NulError> {
         let string = ffi::CString::new(t.into())?;
         Ok(RawSExp(
             unsafe { sexp_eval_string(self.0, string.as_ptr(), -1, ptr::null_mut()) },
             Some(self),
         ))
+    }
+
+    fn symbol_to_string(&self, sexp: &RawSExp) -> String {
+        String(RawSExp(sexp_symbol_to_string(self.0, sexp.0), Some(self)))
     }
 }
 
@@ -284,6 +398,14 @@ impl<'a> Into<SExp<'a>> for RawSExp<'a> {
             Coprod::inject(Pair(self))
         } else if RawContext::stringp(&self) {
             Coprod::inject(String(self))
+        } else if RawContext::integerp(&self) {
+            Coprod::inject(Integer(RawSExp(self.0, None)))
+        } else if RawContext::flonump(&self) {
+            Coprod::inject(Rational(RawSExp(self.0, None)))
+        } else if RawContext::symbolp(&self) {
+            Coprod::inject(Symbol(self))
+        } else if RawContext::exceptionp(&self) {
+            Coprod::inject(Exception(self))
         } else {
             unreachable!()
         };
@@ -306,9 +428,13 @@ impl Context {
         let f = frunk::hlist![
             |b: &'a Bool| &b.0,
             |c: &'a Char| &c.0,
+            |i: &'a Integer| &i.0,
+            |i: &'a Rational<'a>| &i.0,
             |n: &'a Null| &n.0,
             |p: &'a Pair<'a>| &p.0,
-            |s: &'a String<'a>| &s.0
+            |s: &'a String<'a>| &s.0,
+            |s: &'a Exception<'a>| &s.0,
+            |s: &'a Symbol<'a>| &s.0
         ];
 
         let sexp: SExp = self
@@ -321,6 +447,10 @@ impl Context {
     // TODO: Lens please
     pub fn listp<'a>(&self, pair: &'a Pair) -> bool {
         self.0.listp(&pair.0)
+    }
+
+    pub fn make_flonum(&self, i: f64) -> RawSExp {
+        self.0.make_flonum(i)
     }
 }
 
@@ -397,6 +527,38 @@ mod tests {
         assert_eq!(
             context.eval_string("#\\h").unwrap().expect::<Char, _>(),
             Some('h'.into())
+        )
+    }
+
+    #[test]
+    fn test_integer() {
+        let context = Context::default();
+        assert_eq!(
+            context.eval_string("#\\s").unwrap().expect::<Integer, _>(),
+            None
+        );
+        assert_eq!(
+            context.eval_string("(+ 1 3)").unwrap().expect::<Integer, _>(),
+            Some(4.into())
+        )
+    }
+
+    #[test]
+    fn test_rational() {
+        let context = Context::default();
+        assert_eq!(
+            "4.5",
+            format!("{:?}", context.eval_string("4.5").unwrap())
+        );
+
+        assert_eq!(
+            context.eval_string("4.5").unwrap().expect::<Rational, _>(),
+            Some(Rational(context.make_flonum(4.5)))
+        );
+
+        assert_eq!(
+            context.eval_string("(+ 3.0 1.5)").unwrap().expect::<Rational, _>(),
+            Some(Rational(context.make_flonum(4.5)))
         )
     }
 
